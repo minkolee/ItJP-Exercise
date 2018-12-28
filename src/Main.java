722f0227a7702b073625e218003d79d798810492
minkolee <minkolee@gmail.com>
2018-12-28 16:56:36 +0800

    第9章

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        java.util.Date[] dates = new java.util.Date[10];
-        System.out.println(dates[0]);
+        Integer x = 3 + new Integer(5);
+        Integer y = 3;
+        Double z = 3.0; // 这个不对,和声明不同,不能直接将字面int装进Double类
+        int a = new Integer(3) + new Integer(4);
+        System.out.println(a);
+
     }
 
 
+}
+
+class Test {
+    private int[] arrs = new int[10];
+
+    Test() {
+
+    }
+
+    public void addNumber(int i ) {
+        arrs[i] = 3;
+    }
+
+    public void showArr() {
+        System.out.println(Arrays.toString(arrs));
+    }
 }
