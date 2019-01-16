9d03d6afa1c9b3238f46aa82857188792f8500a8
minkolee <minkolee@gmail.com>
2019-01-16 21:37:48 +0800

    泛型程序

--- src/ChuanZhiLearning/polymorph/TestCellPhone.java
+++ src/ChuanZhiLearning/polymorph/TestCellPhone.java
@@ -3,28 +3,32 @@ package ChuanZhiLearning.polymorph;
 public class TestCellPhone {
 
     public static void main(String[] args) {
-        NewPhone n1 = new NewPhone();
-        OldPhone n2 = new OldPhone();
+        Phone p1 = new Phone("MC", 666);
 
-        n1.call();
-        n1.sendMessage();
-        n2.call();
-        n2.sendMessage();
-        n1.playGame();
+        OldPhone p2 = new OldPhone("WC", 777, 999);
 
+
+        System.out.println(p1.id);
+        System.out.println(p1.name);
+        System.out.println(p2.name);
+        System.out.println(p2.id);
+        System.out.println(p2.desk);
+
+    }
+}
+
+class Phone {
+    String name;
+    int id;
+     Phone() {
+    }
+
+    Phone(String name, int id) {
+        this.name = name;
+        this.id = id;
     }
 
 
-
-
-}
-
-
-
-
-
-
-class Phone {
 
     public void call() {
         System.out.println("Is calling");
@@ -39,10 +43,20 @@ class Phone {
 
 interface IPlay {
     void playGame();
+
 }
 
 class OldPhone extends Phone {
+    int desk;
 
+    OldPhone() {
+
+    }
+
+    OldPhone(String name, int id, int desk) {
+        super();
+        this.desk = desk;
+    }
 }
 
 class NewPhone extends Phone implements IPlay {
