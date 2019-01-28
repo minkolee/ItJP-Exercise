b006a113b838fa9de40f7cabfe2475d61f579bce
minkolee <minkolee@gmail.com>
2019-01-28 22:27:37 +0800

    DBUtil的学习

--- src/LearnDBUtils/DBUQuery.java
+++ src/LearnDBUtils/DBUQuery.java
@@ -4,12 +4,9 @@ import JDBCUtil.JDBCConfig;
 import JDBCUtil.JDBCUtil;
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.ArrayHandler;
-import org.apache.commons.dbutils.handlers.ArrayListHandler;
+import org.apache.commons.dbutils.handlers.*;
 
-import javax.naming.event.ObjectChangeListener;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.sql.Connection;
 import java.util.Arrays;
 import java.util.List;
@@ -27,7 +24,13 @@ public class DBUQuery {
 
     public static void main(String[] args) {
 
-        arrayListTest();
+//        arrayTest();
+//        arrayListTest();
+//        bean();
+//        beanList();
+//        column();
+        scalar();
+
         // 第一种方式,ArrayHandler,返回一个数组
     }
 
@@ -53,11 +56,91 @@ public class DBUQuery {
         Connection con = JDBCUtil.getCon(config);
         try {
             List<Object[]> objs = qr.query(con, "SELECT * FROM goods", new ArrayListHandler());
-            System.out.println(objs);
+
+            for (Object[] ob : objs) {
+                System.out.println(Arrays.toString(ob));
+            }
+
         } catch (SQLException ex) {
             System.out.println(ex + "查询失败");
         } finally {
             DbUtils.closeQuietly(con);
         }
     }
+
+    public static void bean() {
+        // 第三种,单个Bean类型对象
+        QueryRunner qr = new QueryRunner();
+        JDBCConfig config = new JDBCConfig("connect.properties");
+        Connection con = JDBCUtil.getCon(config);
+        try {
+            // bean方法的Class <T>指的是传一个类的类文件对象,需要用.class属性来获取,泛型则是返回的类型,就是那个Bean类型
+            Goods objs = qr.query(con, "SELECT * FROM goods", new BeanHandler<Goods>(Goods.class));
+            System.out.print(objs);
+
+
+        } catch (SQLException ex) {
+            System.out.println(ex + "查询失败");
+        } finally {
+            DbUtils.closeQuietly(con);
+        }
+
+    }
+
+    public static void beanList() {
+        // 第四种,返回的是一个List集合,每个元素是Bean对象
+        QueryRunner qr = new QueryRunner();
+        JDBCConfig config = new JDBCConfig("connect.properties");
+        Connection con = JDBCUtil.getCon(config);
+        try {
+            // bean方法的Class <T>指的是传一个类的类文件对象,需要用.class属性来获取,泛型则是返回的类型,就是那个Bean类型
+            List<Goods> objs = qr.query(con, "SELECT * FROM goods", new BeanListHandler<Goods>(Goods.class));
+            System.out.print(objs);
+
+
+        } catch (SQLException ex) {
+            System.out.println(ex + "查询失败");
+        } finally {
+            DbUtils.closeQuietly(con);
+        }
+    }
+
+    public static void column() {
+        // 第四种,返回的是一个List集合,每个元素是Bean对象
+        QueryRunner qr = new QueryRunner();
+        JDBCConfig config = new JDBCConfig("connect.properties");
+        Connection con = JDBCUtil.getCon(config);
+        try {
+            // bean方法的Class <T>指的是传一个类的类文件对象,需要用.class属性来获取,泛型则是返回的类型,就是那个Bean类型
+            List<Object> objs = qr.query(con, "SELECT name FROM goods", new ColumnListHandler<Object>());
+            System.out.print(objs);
+
+
+        } catch (SQLException ex) {
+            System.out.println(ex + "查询失败");
+        } finally {
+            DbUtils.closeQuietly(con);
+        }
+    }
+
+    public static void scalar() {
+        // 第四种,返回的是一个List集合,每个元素是Bean对象
+        QueryRunner qr = new QueryRunner();
+        JDBCConfig config = new JDBCConfig("connect.properties");
+        Connection con = JDBCUtil.getCon(config);
+        try {
+            // bean方法的Class <T>指的是传一个类的类文件对象,需要用.class属性来获取,泛型则是返回的类型,就是那个Bean类型
+            int objs = qr.query(con, "SELECT count(*) FROM goods", new ScalarHandler<Integer>());
+            System.out.print(objs);
+
+
+        } catch (SQLException ex) {
+            System.out.println(ex + "查询失败");
+        } finally {
+            DbUtils.closeQuietly(con);
+        }
+    }
+
+    // MAP的与此类似
+
 }
